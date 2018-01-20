6820bd25a2fb5a40c846c5d339d27c4ca683f102
OUSMANE M. Sadjad <ousmanesadjad@gmail.com>
2018-01-20 18:48:20 +0100

    vue cordova call number extension

--- src/plugins/cordova-plugin-call-number.js
+++ src/plugins/cordova-plugin-call-number.js
@@ -1,12 +1,12 @@
 exports.install = function (Vue, options, cb) {
   document.addEventListener('deviceready', () => {
 
-    if (typeof window.CallNumber === 'undefined') {
+    if (typeof window.plugins.CallNumber === 'undefined') {
       return cb(false)
     }
 
     // pass through the CallNumber object
-    Vue.cordova.CallNumber = window.CallNumber
+    Vue.cordova.CallNumber = window.plugins.CallNumber
 
     return cb(true)
 
